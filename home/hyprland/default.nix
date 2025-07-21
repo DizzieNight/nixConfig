d37ad10bb36a585cf37c4a09e34ef5acc092beaa
DizzieNight <zander@tinytown.au>
2025-07-22 00:05:18 +1000

    enabled hyprland again

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -11,10 +11,10 @@
     config = {
       wayland.windowManager.hyprland = {
         enable = true;
-        # package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-        # portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-        # xwayland.enable = true;
-        # systemd.enable = true;
+        package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+        portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+        xwayland.enable = true;
+        systemd.enable = true;
 
         settings = {
             
