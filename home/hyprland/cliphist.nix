4330ded9241baa411da12daa731c6124321e6ad0
DizzieNight <zander@tinytown.au>
2025-07-22 22:39:12 +1000

    edited cliphist

--- home/hyprland/cliphist.nix
+++ home/hyprland/cliphist.nix
@@ -1,4 +1,7 @@
 {lib, pkgs, config, ...}:
 {
+    home.packages = with pkgs {
+        cliphist
+    };
     services.cliphist.enable = true;
 }
--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -23,7 +23,7 @@
             ];
 
             general = {
-                "$modMod" = "SUPER";
+                "$mod" = "SUPER";
                 layout = "dwindle";
                 gaps_in = 5;
                 gaps_out = 12;
