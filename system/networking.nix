442093cea1fdfcecc6b75db54fdf2d17e8565879
DizzieNight <zander@tinytown.au>
2025-07-24 15:51:35 +1000

    typo

--- system/networking.nix
+++ system/networking.nix
@@ -3,7 +3,7 @@
     networking.networkmanager = {
         enable = true;
         wifi.backend = "iwd";
-    }
+    };
     networking.wireless.iwd = {
         enable = true;
         settings = {
