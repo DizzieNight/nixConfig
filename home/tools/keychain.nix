3be9ca768ad26272d850315c078289a55dd78442
DizzieNight <zander@tinytown.au>
2025-07-28 07:31:22 +1000

    moved pass-secret-service to hm

--- home/tools/keychain.nix
+++ home/tools/keychain.nix
@@ -6,7 +6,7 @@
     # };
     services.pass-secret-service = {
         enable = true;
-        package = pkgs.pass;
+        package = pkgs.pass-secret-service;
         storePath = "${config.home.homeDirectory}./password-store";
     };
     programs.password-store = {
--- system/users.nix
+++ system/users.nix
@@ -8,7 +8,6 @@
         dbus
         bridge-utils
         libsecret
-        pass-secret-service
     # Apps
         protonmail-bridge-gui
         putty
