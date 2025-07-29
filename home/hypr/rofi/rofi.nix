94c8baf850a427a8c6b0daf8898804096128c2ec
DizzieNight <zander@tinytown.au>
2025-07-29 11:19:40 +1000

    symlinked rofi

--- home/hypr/rofi/rofi.nix
+++ home/hypr/rofi/rofi.nix
@@ -1,8 +1,41 @@
 {lib, pkgs, config, ...}:
+let
+  rofiPath = ./rofiConfig;
+in 
 {
-    programs.rofi = {
-        enable = true;
-        configPath = "./config.rasi";
-        theme = lib.mkForce "./style.rasi";
-    };
+    # programs.rofi = {
+    #     enable = true;
+    #     modes = [
+    #         "window"
+    #         "drun"
+    #         "run"
+    #         "ssh"
+    #     ];
+    #     pass = {
+    #         enable = true;
+    #         stores = [
+    #             "${config.home.homeDirectory}/.password-store"
+    #         ];
+    #     };
+    #     plugins = [
+    #         pkgs.rofi-calc
+    #     ];
+    #     location = "center";
+    #     font = "Jetbrains Mono";
+    #     cycle = true;
+    #     # extraConfig = [
+    #     #     "show-icons: true"
+    #     #     "display-drun: "Apps""
+    #     #     timeout = {
+    #     #         action= "kb-cancel";
+    #     #         delay=  0;
+    #     #     };
+    #     #     filebrowser = {
+    #     #         directories-first= true;
+    #     #         sorting-method=    "name";
+    #     #     };
+    #     # ];
+    #     # theme = lib.mkForce "${config.home.homeDirectory}/.config/rofi/style.rasi";
+    # };
+    xdg.configFile."rofi".source = config.lib.file.mkOutOfStoreSymlink rofiPath; 
 }
